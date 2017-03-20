d3225b76311a21089fbb29d34d3422d64465f64b
Lucas Natraj <lnatraj@slb.com>
2017-03-20 09:56:52 -0700

    try to fix import

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Package gogcloud is a set of packages that allow working with the gcloud libraries
+package gogcloud
+
+// blank imports help docs.
+import (
+	// emulators package
+	_ "github.com/blue-army/go-gcloud/emulators"
+)
