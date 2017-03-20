39b845c4b1ab04feaa60324b7456f51766a9b672
Lucas Natraj <lnatraj@slb.com>
2017-03-20 10:47:34 -0700

    change

--- doc.go
+++
@@ -1,8 +0,0 @@
-// Package gcloud is a set of packages that allow working with the gcloud libraries
-package gcloud
-
-// blank imports help docs.
-import (
-	// emulators package
-	_ "github.com/blue-army/go-gcloud/emulators"
-)
---
+++ version.go
@@ -0,0 +1,5 @@
+// Package gcloud is a set of packages that allow working with the gcloud libraries
+package gcloud
+
+// VERSION is the current version of the package
+const VERSION = "1.0.0"
